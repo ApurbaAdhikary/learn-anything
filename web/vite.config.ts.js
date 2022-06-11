827f8e373d85e71eaaf6789391e66adc106965ad
Nikita Voloboev <nikita.voloboev@gmail.com>
2022-06-11 20:07:59 +0200

    try

---
+++ web/vite.config.ts.js
@@ -0,0 +1,14 @@
+// vite.config.ts
+import solid from "solid-start";
+import netlify from "solid-start-netlify";
+var vite_config_default = {
+  plugins: [
+    solid({
+      adapter: netlify()
+    })
+  ]
+};
+export {
+  vite_config_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsidml0ZS5jb25maWcudHMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbImltcG9ydCBzb2xpZCBmcm9tIFwic29saWQtc3RhcnRcIlxuaW1wb3J0IG5ldGxpZnkgZnJvbSBcInNvbGlkLXN0YXJ0LW5ldGxpZnlcIlxuXG5leHBvcnQgZGVmYXVsdCB7XG4gIHBsdWdpbnM6IFtcbiAgICBzb2xpZCh7XG4gICAgICBhZGFwdGVyOiBuZXRsaWZ5KClcbiAgICB9KVxuICBdXG59XG4iXSwKICAibWFwcGluZ3MiOiAiO0FBQUE7QUFDQTtBQUVBLElBQU8sc0JBQVE7QUFBQSxFQUNiLFNBQVM7QUFBQSxJQUNQLE1BQU07QUFBQSxNQUNKLFNBQVMsUUFBUTtBQUFBLElBQ25CLENBQUM7QUFBQSxFQUNIO0FBQ0Y7IiwKICAibmFtZXMiOiBbXQp9Cg==
