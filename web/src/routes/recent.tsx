9fe243261952eb9631ceefbb22e4681c1e9426ff
Nikita Voloboev <nikita.voloboev@gmail.com>
2022-06-15 01:54:13 +0200

    edgedb

--- web/src/routes/recent.tsx
+++ web/src/routes/recent.tsx
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "redaxios"
 import { useRouteData } from "solid-start/router"
 import { createServerResource } from "solid-start/server"
 import PageWrap from "~/components/PageWrap"
@@ -7,6 +7,8 @@ import Nav from "~/components/Nav"
 
 export function routeData() {
   return createServerResource(async () => {
+    // console.log(window, "is window defined?")
+    // console.log("hello")
     // TODO: make it read from env var, for now localhost
     // Run server from `server` folder.
     const res = await axios.get("http://localhost:8080/links")
@@ -17,15 +19,16 @@ export function routeData() {
 
 export default function Recent() {
   const links = useRouteData<ReturnType<typeof routeData>>()
+  console.log(links())
 
   return (
     <PageWrap>
       <Nav activePage={"Recent"} />
       <div>
         <ul>
-          {links()?.map((item) => (
+          {/* {links()?.map((item) => (
             <li>{item}</li>
-          ))}
+          ))} */}
         </ul>
       </div>
     </PageWrap>
