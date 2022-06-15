694fcae72001203e4b36768504d283c200a42090
Nikita Voloboev <nikita.voloboev@gmail.com>
2022-06-15 02:08:56 +0200

    query

--- web/src/routes/recent.tsx
+++ web/src/routes/recent.tsx
@@ -1,34 +1,37 @@
-import axios from "redaxios"
 import { useRouteData } from "solid-start/router"
 import { createServerResource } from "solid-start/server"
 import PageWrap from "~/components/PageWrap"
 import Nav from "~/components/Nav"
-// import e, {$infer} from '../../dbschema/edgeql-js';
+import e, { $infer } from "../../dbschema/edgeql-js"
+import { createClient } from "edgedb"
+
+export const client = createClient()
+
+const getLinks = e.select(e.Link, () => ({
+  title: true,
+  url: true,
+}))
+
+export type getLinks = $infer<typeof getLinks>
 
 export function routeData() {
   return createServerResource(async () => {
-    // console.log(window, "is window defined?")
-    // console.log("hello")
-    // TODO: make it read from env var, for now localhost
-    // Run server from `server` folder.
-    const res = await axios.get("http://localhost:8080/links")
-    if (res.data && res.data.links) return res.data.links
-    return []
+    const links = await getLinks.run(client)
+    return links
   })
 }
 
 export default function Recent() {
   const links = useRouteData<ReturnType<typeof routeData>>()
-  console.log(links())
 
   return (
     <PageWrap>
       <Nav activePage={"Recent"} />
       <div>
         <ul>
-          {/* {links()?.map((item) => (
-            <li>{item}</li>
-          ))} */}
+          {links()?.map((link) => (
+            <li>{link.title}</li>
+          ))}
         </ul>
       </div>
     </PageWrap>
