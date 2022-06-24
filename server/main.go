7dc9d09b10802a09f5aed9e38c9e1e29a23e2ed0
Nikita Voloboev <nikita.voloboev@gmail.com>
2022-06-24 20:46:01 +0200

    .

--- server/main.go
+++ server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	edgedb "github.com/edgedb/edgedb-go"
 )
 
 // Gets all links saved for user as JSON
@@ -21,17 +24,43 @@ func links(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, string(jsonLinks))
 }
 
-func main() {
-	http.HandleFunc("/links", links)
+func edgedbSetup() {
+	opts := edgedb.Options{
+		Database:    "edgedb",
+		User:        "edgedb",
+		Concurrency: 4,
+	}
 
+	ctx := context.Background()
+	client, err := edgedb.CreateClient(ctx, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	var result string
+	err = client.QuerySingle(ctx, "SELECT 'hello EdgeDB!'", &result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result)
+
+}
+
+func serve() {
+	http.HandleFunc("/links", links)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
-
 	log.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	edgedbSetup()
 }
